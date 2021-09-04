6725b1ff3c9c2ea8569121a8b0fc72c1fd150481
Pramit Dutta <pramit.b.dutta@gmail.com>
2021-09-04 14:35:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_dirlist(_rootdir):
 
 
 # Get subfolders recursively
-os.chdir('..')
+# os.chdir('..')
 rootdir = 'python-sdk'
 packages = [d.replace('/', '.').replace('{}.'.format(rootdir), '') for d in get_dirlist(rootdir)]
 
